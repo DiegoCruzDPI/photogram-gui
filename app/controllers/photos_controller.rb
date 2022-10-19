dd2aed16d0b5e9ba15362ae57b21625ac92da63d
DiegoCruz02 <a.diegocruz02@gmail.com>
2022-10-19 16:43:57 +0000

    Delete for photos works!!!

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,8 +1,34 @@
 class PhotosController <ApplicationController
-def index
-  matching_photos = Photo.all
+  def index
+    matching_photos = Photo.all
 
-  @list_of_photos = matching_photos.order({ :created_at => :desc})
-render({ :template => "photo_templates/index.html.erb"})
-end
+    @list_of_photos = matching_photos.order({ :created_at => :desc})
+    render({ :template => "photo_templates/index.html.erb"})
+  end
+
+  def show
+    #Parameters: {"path_id"=>"777"}
+    url_id = params.fetch("path_id")
+
+    matching_photos = Photo.where({ :id => url_id})
+
+    @the_photo = matching_photos.at(0)
+
+    render({ :template => "photo_templates/show.html.erb"})
+  end
+
+  def baii
+    the_id = params.fetch("toast_id")
+
+    matching_photos = Photo.where({ :id => the_id})
+
+    the_photo = matching_photos.at(0)
+
+    the_photo.destroy
+
+    #render({ :template => "photo_templates/baii.html.erb"})
+    #we can also redirect if the page we end up on is not really needed!!!!!
+    redirect_to("/photos")
+  end
+
 end
